0ba443fe0483969263f6c9c811c0ad3549a52042
yg_zinger <yg_zinger@163.com>
2018-01-09 23:49:08 +0800

    '-'

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # parcel_demo
 
 个人的 `parcel` 学习笔记
+
+- amfe-flexible
+- jquery
+- normalize.css
+
+```git
+npm install --save amfe-flexible normalize.css jquery
+```
--- src/script/index.js
+++ src/script/index.js
@@ -1,2 +1,11 @@
+import 'normalize.css'
 import '../style/index.scss'
 
+import 'amfe-flexible'
+import $ from 'jquery'
+
+$(function() {
+  const str = 'hello parcel !';
+  console.log(str);
+  $('#parcel').append('<h1>'+str+'</h1>');
+});
