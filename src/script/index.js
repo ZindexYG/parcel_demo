f8b53f0062bf175fa80ad4c8e6ecdfbb43633cf5
ZindexYG <yg_zinger@163.com>
2018-05-25 18:28:11 +0800

    up

--- src/script/index.js
+++ src/script/index.js
@@ -1,11 +1,13 @@
 import 'normalize.css'
 import '../style/index.scss'
 
-import 'amfe-flexible'
+// import 'amfe-flexible'
 import $ from 'jquery'
 
 $(function() {
   const str = 'hello parcel !';
   console.log(str);
   $('#parcel').append('<h1>'+str+'</h1>');
+
+
 });
